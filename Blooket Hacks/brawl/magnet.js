70c1678f18ffa2e0e8580bb372ca97f03ef0cab5
playstation451 <98996547+playstation452@users.noreply.github.com>
2024-03-06 19:13:55 -0500

    Update magnet.js

--- Blooket Hacks/brawl/magnet.js
+++ Blooket Hacks/brawl/magnet.js
@@ -2,3 +2,6 @@
     const cheat = (async () => {
         Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.state.game.scene.physics.world.colliders._active.find(x => x.collideCallback?.toString().includes('0x5dc)')).collideCallback({ active: true }, { active: true, setActive() { }, setVisible() { } });
     });
+    
+    cheat(); // Directly call the cheat function without checking for updates
+})();
