4e68e6e802cebfa7246c710828afb02bd17fc459
playstation451 <98996547+playstation452@users.noreply.github.com>
2024-03-06 19:13:34 -0500

    Update invincibility.js

--- Blooket Hacks/brawl/invincibility.js
+++ Blooket Hacks/brawl/invincibility.js
@@ -2,3 +2,6 @@
     const cheat = (async () => {
         for (const collider of Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.state.game.scene.physics.world.colliders._active.filter(x => x.callbackContext?.toString().includes('invulnerableTime') || x.callbackContext?.toString().includes('dmgCd'))) collider.collideCallback = () => { };
     });
+
+     cheat(); // Directly call the cheat function without checking for updates
+})();
