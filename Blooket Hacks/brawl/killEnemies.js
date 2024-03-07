a2e2737a2abba53fd0ca92b6d3c92202321002f4
playstation451 <98996547+playstation452@users.noreply.github.com>
2024-03-06 19:13:43 -0500

    Update killEnemies.js

--- Blooket Hacks/brawl/killEnemies.js
+++ Blooket Hacks/brawl/killEnemies.js
@@ -2,3 +2,6 @@
     const cheat = (async () => {
         Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.state.game.scene.physics.world.bodies.entries.forEach(x => x?.gameObject?.receiveDamage?.(x.gameObject.hp, 1));
     });
+
+     cheat(); // Directly call the cheat function without checking for updates
+})();
