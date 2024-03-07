10d5b4dc20a6625f7991a630e03bcd842a6787ba
playstation451 <98996547+playstation452@users.noreply.github.com>
2024-03-06 19:43:02 -0500

    Update gui.js

--- Blooket Hacks/gui.js
+++ Blooket Hacks/gui.js
@@ -3949,23 +3949,3 @@
             });
         }
     });
-    let img = new Image;
-    img.src = "https://raw.githubusercontent.com/05Konz/Blooket-Cheats/main/autoupdate/timestamps/gui.png?" + Date.now();
-    img.crossOrigin = "Anonymous";
-    img.onload = function() {
-        const c = document.createElement("canvas");
-        const ctx = c.getContext("2d");
-        ctx.drawImage(img, 0, 0, this.width, this.height);
-        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
-        for (let i = 0; i < data.length; i += 4) {
-            let char = String.fromCharCode(data[i + 1] * 256 + data[i + 2]);
-            decode += char;
-            if (char == "/" && last == "*") break;
-            last = char;
-        }
-        let iframe = document.querySelector("iframe");
-        const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "([\s\S]+?)"/);
-        if (parseInt(time) <= 1707871479125 || iframe.contentWindow.confirm(error)) cheat();
-    }
-    img.onerror = img.onabort = () => (img.src = null, cheat());
-})();
